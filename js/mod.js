ca52efe59f295b58b235478c4b067d4e89764412
someone1333 <ai18782709ai@gmail.com>
2022-11-28 07:14:11 +0900

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,6 +1,6 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
+	name: "The increment Tree",
+	id: "incrementing",
 	author: "nobody",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
